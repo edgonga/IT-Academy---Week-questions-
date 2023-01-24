ffdb978096a2b61732911cbb887887a1bd1cc7df
edgonga <gonzalez.edu.duc@gmail.com>
2023-01-24 14:42:21 +0100

    week challenge

--- ZeroReplacerWithLength/repository.js
+++ ZeroReplacerWithLength/repository.js
@@ -11,33 +11,57 @@
 
 const zeroReplacer = (numberString) => {
     if (typeof(numberString === String)) {
-                
+        
         numberArray = Array.from(numberString)
         //let zeroLocation = [,]
         let zeroCounter = 1
-        for (let index = 0; index > numberString; index++) {
+        let previousIsAZero = false // Try it with this variable
+                                    // Added inside the conditionals
+                                    // And reset it to true when the current
+                                    // number is 0, like this, in the next loop
+                                    // will represent the previous one
+                                    
+        for (let index = 0; index <= numberString.length; index++) {
             
             // let element = Number  // if we introduce the num variable as a number, but is not working properly as Number data type
-            if (numberString[index] === "0") {
-                
-                if (numberString[index] - 1 === "0") {
-                    zeroCounter += 1
-                    console.log("Zero Counter --> " + zeroCounter);
-                    
-                } else {numberArray.splice(index, 0, zeroCounter)
-                    console.log(numberArray);
-                
-                }   
-            } if (numberString[index] !== "0") {
-                zeroCounter = 1
-            }
 
-            
-        }
+
+    
+                
+                if (numberString[index] !== "0" && numberString[index -1] === "0") {
+                    numberArray.splice(index, zeroCounter, zeroCounter.toString())
+                    console.log("Loop ----> " + index);
+                    console.log("previous " + numberString[index -1]);
+                    console.log("current " + numberString[index]);
+                    console.log("ZeroCounter " + zeroCounter);
+                    console.log("SSSSSSSSSSSSSSSS");
+                }   
+                
+                if (numberString[index] !== "0" && numberString[index -1] !== "0") {
+                    zeroCounter = 1
+                    console.log("Loop ----> " + index);
+                    console.log("previous " + numberString[index -1]);
+                    console.log("current " + numberString[index]);
+                    console.log("ZeroCounter " + zeroCounter);
+                    console.log("QQQQQQQQQQQQQQQQQ");
+                }
+
+                if (numberString[index] === "0" && numberString[index -1] === "0") {
+                    zeroCounter += 1
+                    console.log("Loop ----> " + index);
+                    console.log("previous " + numberString[index -1]);
+                    console.log("current " + numberString[index]);
+                    console.log("ZeroCounter " + zeroCounter);
+                    console.log("HHHHHHHHHHHHHHHH");
+
+                }
+
+            }
         numberArray = numberArray.join("")
         return numberArray;
-} else {
+    } else {
     return "El input debe ser un string"
 }}
 
 zeroReplacer("1234500362000440")
+
--- ZeroReplacerWithLength/zeroReplacer.js
+++ ZeroReplacerWithLength/zeroReplacer.js
@@ -4,28 +4,24 @@ const zeroReplacer = (numberString) => {
         numberArray = Array.from(numberString)
         //let zeroLocation = [,]
         let zeroCounter = 1
-        for (let index = 0; index > numberString; index++) {
-            
-            // let element = Number  // if we introduce the num variable as a number, but is not working properly as Number data type
-            if (numberString[index] === "0") {
-                
-                if (numberString[index] - 1 === "0") {
-                    zeroCounter += 1
-                    console.log("Zero Counter --> " + zeroCounter);
-                    
-                } else {numberArray.splice(index, 0, zeroCounter)
-                    console.log(numberArray);
-                
-                }   
-            } if (numberString[index] !== "0") {
-                zeroCounter = 1
+        for (let index = 0; index <= numberString.length; index++) {
+
+            if (numberString[index] === "0" && numberString[index -1] === "0") {
+                zeroCounter += 1
             }
 
-            
+            if (numberString[index] !== "0" && numberString[index -1] === "0") {
+                numberArray.splice(index, zeroCounter, zeroCounter.toString())
+            }
+
+            if (numberString[index] !== "0" && numberString[index -1] !== "0") {
+                zeroCounter = 1
+            }
+                        
         }
         numberArray = numberArray.join("")
         return numberArray;
-} else {
+    } else {
     return "El input debe ser un string"
 }}
 
